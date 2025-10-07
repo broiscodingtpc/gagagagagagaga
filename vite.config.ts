116d069198d4edf8c32779493fda798192429ffc
robertpetrea <robertpetrea@example.com>
2025-10-08 00:52:54 +0300

    FIX VITE OUTPUT: Use absolute path for outDir to ensure files are generated correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   root: "web",
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "web/dist"),
     emptyOutDir: true,
   },
   server: { 
